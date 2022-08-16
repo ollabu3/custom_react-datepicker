1345c794625d1a9ef29c6e79c09d678ffbfd5013
ollabu3 <ollabu3@gmail.com>
2022-08-16 18:15:14 +0900

    global css 추가

---
+++ src/styles/theme.ts
@@ -0,0 +1,17 @@
+const color = {
+  bgDefault: "#ffffff",
+  bgMain: "#3e4ede",
+  bgBorder: "#dfdfe3",
+  txMain: "#3e4ede",
+  txDefault: "#000000",
+};
+
+const text = {
+  title: "20px",
+  main: "16px",
+  sub: "14px",
+};
+
+const theme = { color, text };
+
+export default theme;
