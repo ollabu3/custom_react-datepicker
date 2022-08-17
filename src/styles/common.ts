9cf302eec02486055895b78247903c81c5c7eb00
ollabu3 <ollabu3@gmail.com>
2022-08-17 17:50:47 +0900

    global css 변경

---
+++ src/styles/common.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+`;
+
+export const Title = styled.h2`
+  font-size: var(--sz-title);
+`;
